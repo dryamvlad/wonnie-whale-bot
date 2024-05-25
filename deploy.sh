58bbf899d8de909f9ff9123dccc074c2f9af2875
dryamvlad <dryam.vlad@gmail.com>
2024-05-25 12:22:47 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CR_PAT=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
+
 
 docker build . -t ghcr.io/dryamvlad/jgf-scanner:main --platform linux/amd64 && \
 echo $CR_PAT | docker login ghcr.io -u dryamvlad --password-stdin && \
